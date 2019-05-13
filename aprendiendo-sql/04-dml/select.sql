2ec90bae07cbeb4545b99cd6fe65f6b1e895c6b8
vmanuel <v.manuel.nago@gmail.com>
2019-05-13 16:50:11 -0500

    Commit 13/05/19

---
+++ aprendiendo-sql/04-dml/select.sql
@@ -0,0 +1,11 @@
+/**
+ * Author:  VManuel
+ * Created: 13-may-2019
+ */
+
+#MOSTRAR TODOS LOS REGISTROS/FILAS DE UNA TABLA#
+SELECT email,nombre,apellidos FROM usuarios;
+
+
+#OPERADORES ARITMETICOS#
+SELECT  email,(4+7) AS 'Operacion' FROM usuarios;
